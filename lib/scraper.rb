5a0ba7671933cc390bba740af03ab5561e2b790b
Owen Cruse <ocruse7@gmail.com>
2021-01-15 01:11:05 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -2,4 +2,4 @@ require 'nokogiri'
 require 'open-uri'
 
 doc = Nokogiri::HTML(open("https://flatironschool.com/"))
-doc.css(".headline-260IBN").text
+doc.css("".inlineMobileLeft-2Yo002.imageTextBlockGrid3-2XAK6G"
